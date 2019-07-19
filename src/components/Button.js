497325eb90e45bc47f22f1c759ebd0747e92b714
Jordan Pillai <admin@jordans-mbp.mynet>
2019-07-19 17:00:13 +0100

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Button = props => (
+  <button onClick={props.onClick} style={props.style}>
+    {props.label && <span>{props.label}</span>}
+  </button>
+);
+export default Button;
